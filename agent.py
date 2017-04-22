9ea8d9567ee2ced58ae5b54ee3161acab7a8e49f
Eren <eren@ip-114-94.wireless.oberlin.edu>
2017-04-22 16:50:34 -0400

    adding qlearning to agent

--- agent.py
+++ agent.py
@@ -1,22 +1,95 @@
 # represents an agent within the environment
 
+import random
+
 class Agent():
 
     # initializes the agent's environment and state
-    def __init__(self, environment, x, y):
+    def __init__(self, environment, state):
         self.environment = environment
-        self.x = x
-        self.y = y
-        self.carry = False
-
-    # returns the agent's state
-    def get_state(self):
-        return [self.x, self.y, self.carry]
+        self.state = state
+        self.qtable = dict()
+        self.cumulative_reward = 0
 
     # returns the possible actions the agent can do
     def get_actions(self):
         return environment.get_agent_actions(self)
 
     # returns the reward for doing the given action
-    def do_action(action):
+    # updates state and qtable
+    # takes in the value of alpha for updateing qtable
+    def do_action(self, action, alpha):
+        new_state, reward = environment.do_action(self, action)
+        self.update_state(new_state, )
         return environment.do_action(self, action)
+
+    # returns the reward for action without actually doing it
+    def mock_action(self, action):
+        return environment.mock_action(self, action)
+
+    # extracts an entry from the q-table, handling uninitialized entires
+    def get_q(state, action):
+        if state not in self.qtable:
+            return 0
+        elif action not in self.qtable[state]:
+            return 0
+        else:
+            return qtable[state][action]
+
+    # updates the state and qtable
+    def update_state(action, next_state, alpha):
+        prev_component = (1 - alpha) * self.get_q(state, action)
+        self.state = next_state
+        new_q = self.get_q(next_state, get_exploitative_action)
+
+    # returns a random action
+    def get_explorative_action():
+        actions = self.get_actions()
+        r = random.randrange(len(actions))
+        return actions[r]
+
+    # returns the action with the highest q value given the state
+    def get_exploitative_action():
+        actions = self.get_actions()
+        result = None
+        highest = -float("inf")
+
+        for action in actions:
+            if self.q(qtable, state, action) > highest:
+                highest = self.q(qtable, state, action)
+                result = action
+        
+        return result
+
+    # picks an action using the epsilon-greedy approach
+    def pick_action_epsilon(epsilon):
+        r = random.random()
+        if r < epsilon:
+            return self.get_explorative_action()
+        else:
+            return self.get_exploitative_action()
+
+    # picks an action using softmax
+    def pick_action_softmax(tau):
+        r = random.random()
+        state = self.state
+        actions = self.get_actions()
+
+        total = 0
+        for action in actions:
+            total += math.e ** (self.q(qtable, state, action) / TAU)
+
+        if total <= 1:
+            return self.get_explorative_action() 
+            
+        probs = dict()
+        for action in actions:
+            probs[action] = (math.e ** (self.q(qtable, state, action) / TAU)) / total
+        
+        current = 0
+        for action in probs:
+            current += probs[action]
+            if r < current:
+                return action
+
+        return self.get_explorative_action()
--- qlearning.py
+++ qlearning.py
@@ -1,15 +1,16 @@
 # qlearning.py
-# Eren Guendelsberger
-# Spring 2017
 # Uses q-learning to perform in a given environment
+# Updated from hw4 to support multiple agents in one environmenmt
 
-from grid import Grid
 import matplotlib.pyplot as plt
 import random
 import math
 
 # number of iterations to run q-learning
 NUM_EPISODES = 1000
+
+# number of steps to run each episode
+NUM_STEPS = 1000
 
 # higher values include information from more steps
 DISCOUNT_FACTOR = 0.99
@@ -24,9 +25,9 @@ TAU = 0.01
 # q-learns the grid environment with epsilon-greedy and softmax
 # approaches for selecting actions and graphs the results
 def main():
-    epsilon1 = qlearning(Grid(), NUM_EPISODES, DISCOUNT_FACTOR, EPSILON_MODE, EPSILON1)
-    epsilon2 = qlearning(Grid(), NUM_EPISODES, DISCOUNT_FACTOR, EPSILON_MODE, EPSILON2)
-    softmax = qlearning(Grid(), NUM_EPISODES, DISCOUNT_FACTOR, SOFTMAX_MODE)
+    epsilon1 = qlearning(Environment(), NUM_EPISODES, DISCOUNT_FACTOR, EPSILON_MODE, EPSILON1)
+    epsilon2 = qlearning(Environment(), NUM_EPISODES, DISCOUNT_FACTOR, EPSILON_MODE, EPSILON2)
+    softmax = qlearning(Environment(), NUM_EPISODES, DISCOUNT_FACTOR, SOFTMAX_MODE)
     
     episodes = range(1, NUM_EPISODES + 1) 
     plt.scatter(episodes, epsilon1, s=20, label="epsilon 0.05")
@@ -40,11 +41,16 @@ def main():
 
 # Returns a list of cumulative rewards for each of the num_episodes it runs
 def qlearning(environment, num_episodes, discount_factor, mode, epsilon=0):
+    agents = environment.get_all_agents()
+
+    # TODO: for now we are just summing the results for each individual angent, perhaps it might be worthwhile to graph separately?
     results = []
     qtable = dict()
+    for agent in agents:
+        qtable[agent] = dict()
 
     for i in range(num_episodes):
-        results.append(qlearn_episode(environment, qtable, discount_factor, mode, epsilon))
+        results.append(qlearn_episode(agents, qtable, discount_factor, mode, epsilon))
 
     return results
 
@@ -52,19 +58,27 @@ def qlearning(environment, num_episodes, discount_factor, mode, epsilon=0):
 # While we don't reach an absorbing state,
 # pick an action based on the Q-table and
 # update the table and comulative reward
-def qlearn_episode(environment, qtable, discount_factor, mode, epsilon):
-    cumulative_reward = 0 
-    state = environment.generateStartState()
-   
-    steps = 0
-    while state != environment.ABSORBING_STATE:
-        action = pick_action(environment, qtable, state, mode, epsilon)
-        next_state = environment.generateNextState(state, action)
-        reward = environment.generateReward(state, action)
-        cumulative_reward += (discount_factor ** steps) * reward 
-        update_qtable(environment, qtable, steps+1, discount_factor, state, action, next_state, reward)
-        state = next_state
-        steps += 1 
+def qlearn_episode(agents, qtable, discount_factor, mode, epsilon):
+    # dictionary mapping each agent to its cumulative reward
+    cumulative_reward = dict()
+    # dictionary mapping each agent to its state
+    state = dict() 
+    # initialize the state and reward of  each agent
+    for agent in agents:
+        cumulative_reward[agent] = 0
+        state[agent] = agent.get_state()
+
+    # run the simulation
+    for i in range(NUM_STEPS):
+        # give each agent a turn
+        for agent in agents:
+            action = pick_action(agent, agent.get_actions(), qtable[agent], mode, epsilon)
+            reward = agent.do_action(action) 
+            next_state = agent.get_state() 
+            cumulative_reward[agent] += (discount_factor ** steps) * reward 
+            update_qtable(environment, qtable, steps+1, discount_factor, state, action, next_state, reward)
+            state = next_state
+            steps += 1 
 
     return cumulative_reward
 
@@ -81,61 +95,11 @@ def update_qtable(environment, qtable, steps, discount_factor, state, action, ne
     qtable[state][action] = previous + update
 
 # picks an action using either softmac or the epsilon-greedy approach
-def pick_action(environment, qtable, state, mode, epsilon):
-    r = random.random()
-
-    if mode == SOFTMAX_MODE:
-        total = 0
-        for action in environment.actions:
-            total += math.e ** (q(qtable, state, action) / TAU)
-
-        if total == 0:
-            return get_explorative_action(environment.actions) 
-            
-        probs = dict()
-        for action in environment.actions:
-            probs[action] = (math.e ** (q(qtable, state, action) / TAU)) / total
-        
-        current = 0
-        for action in probs:
-            current += probs[action]
-            if r < current:
-                return action
-
-    elif mode == EPSILON_MODE:
-        exploitative_action = get_exploitative_action(state, environment.actions, qtable)
-        explorative_action = get_explorative_action(environment.actions)
-        if r < epsilon:
-            return explorative_action
-        else:
-            return exploitative_action
-
-# returns the action with the highest q value given the state
-def get_exploitative_action(state, actions, qtable):
-    result = None
-    highest = -float("inf")
-
-    for action in actions:
-        if q(qtable, state, action) > highest:
-            highest = q(qtable, state, action)
-            result = action
-    
-    return result
-
-# returns a random action
-def get_explorative_action(actions):
-    r = random.randrange(len(actions))
-    return actions[r]
-
-# extract an entry from the q-table, handling uninitialized cells
-def q(qtable, state, action):
-    if state not in qtable:
-        return 0 
-    elif action not in qtable[state]:
-        return 0 
-    else:
-        return qtable[state][action]
+def pick_action(agent, mode, tune):
+    if mode == EPSILON_MODE:
+        return agent.pick_action_epsilon(tune)
+    elif mode == SOFTMAX_MODE:
+        return agent.pick_action_softmax(tune)
 
 if __name__ == "__main__":
     main()
-
