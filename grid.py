42252503172f2f5321d452bf4e43dc66e5a81d19
James Capuder <jamescapuder@ip-65-73.wireless.oberlin.edu>
2017-04-22 15:53:56 -0400

    more grid work

--- grid.py
+++ grid.py
@@ -3,7 +3,7 @@
 class Grid():
     SPAWN = 1
     SOURCE = 2
-
+    AGENT = 3
     
     def __init__(self):
         #initialize empty board
@@ -14,23 +14,40 @@ class Grid():
         #set spawn and source locations
         self.board[0][0] = Grid.SPAWN
         self.board[30][40] = Grid.SOURCE
+
         #initialize list of tuple-states
         self.states = []
         self.createStates()
-        
 
+        self.transitions = {}
+        self.actions = ["up", "down", "left", "right"]
+        self.transitionMap()
+
+    def isValidAction(self, state, action):
+        return self.transitions[state][action] not in [Grid.SOURCE, Grid.AGENT]
+
+
+    def transitionMap(self):
+        for state in self.states:
+            self.transitions[state] = self.getAdjacent(state)
+            
+    
+    def getAdjacent(self, coord):
+        return {"right": (coord[0]+1, coord[1]), "left": (coord[0]-1, coord[1]), "down": (coord[0], coord[1]+1), "up": (coord[0], coord[1]-1)}
         
     def createStates(self):
-        for x in range(Grid.WIDTH):
-            for y in range(Grid.HEIGHT):
-                if (x, y) not in Grid.OBSTACLES:
-                    self.states.append((x, y))
-        self.states.append(Grid.ABSORBING_STATE)
+        for x in range(0,50):
+            for y in range(0,50):
+                self.states.append((x, y))
+        #self.states.append(Grid.ABSORBING_STATE)
 
+    
         
         
         
 
 if __name__=="__main__":
     g = Grid()
-    print(g.board)
+    for l in g.board:
+        print(*l)
+    print(g.transitions)
