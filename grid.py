79aefdfca8f646c500aef3cae53a1fd3366935bf
James Capuder <jamescapuder@ip-65-73.wireless.oberlin.edu>
2017-04-22 15:14:31 -0400

    set locations

--- grid.py
+++ grid.py
@@ -1,11 +1,22 @@
 
 
 class Grid():
+    SPAWN = 1
+    SOURCE = 2
+
+    
     def __init__(self):
+        #initialize empty board
         self.board = []
         for i in range(0,50):
             self.board.append([0]*50)
-        self.board[1][1] = 1
+
+        #set spawn and source locations
+        self.board[0][0] = Grid.SPAWN
+        self.board[30][40] = Grid.SOURCE
+
+        
+        
         
         
 
