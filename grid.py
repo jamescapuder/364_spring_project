59630e1fb75b7140bab693281b5ef5aedeb6f897
James Capuder <jamescapuder@ip-65-73.wireless.oberlin.edu>
2017-04-22 17:22:21 -0400

    baby state class

--- grid.py
+++ grid.py
@@ -1,20 +1,23 @@
-TEST = False
+import agent
 
 class Grid():
     SPAWN = 1
     SOURCE = 2
     AGENT = 3
     
-    def __init__(self):
+    def __init__(self, agents):
         #initialize empty board
         self.board = []
         for i in range(0,50):
             self.board.append([0]*50)
 
+        self.agents = agents
         #set spawn and source locations
         self.board[0][0] = Grid.SPAWN
         self.board[30][40] = Grid.SOURCE
         self.source = (30,40)
+        self.spawn = (0,0)
+        
         #initialize list of tuple-states
         self.states = []
         self.createStates()
@@ -23,12 +26,21 @@ class Grid():
         self.actions = ["up", "down", "left", "right"]
         self.transitionMap()
 
-    def isValidAction(self, state, action):
-        in_actions =  action in self.transitions[state].keys()
-        if in_actions:
-            return self.transitions[state][action] != self.source
-        return False
+    def get_all_agents(self):
+        return self.agents
+    
+    def do_action(self, agent, action):
+        x, y = agent.state
+        new_x, new_y =  self.transitions[agent.state][action]
+        self.board[x][y] = 0
+        self.board[new_x][new_y] = Grid.AGENT
 
+    def generate_reward(self, agent_state, action):
+        if self.getNextState(agent_state.coords, action) in list(self.getAdjacent(source)):
+            
+    
+    def get_agent_actions(self, agent):
+        return list(self.transitions[agent.state].keys())
         
     def transitionMap(self):
         for state in self.states:
@@ -37,7 +49,7 @@ class Grid():
     def getAdjacent(self, coord):
         adj =  {"right": (coord[0]+1, coord[1]), "left": (coord[0]-1, coord[1]), "down": (coord[0], coord[1]+1), "up": (coord[0], coord[1]-1)}
         for k in list(adj.keys() ):
-            if (0 > adj[k][0]) or (adj[k][0]>49) or (0>adj[k][1]) or (adj[k][1]>49):
+            if (0 > adj[k][0]) or (adj[k][0]>49) or (0>adj[k][1]) or (adj[k][1]>49) or adj[k] in [self.source].extend([agent.state for agent in self.agents]):
                 del adj[k]
         return adj
     
---
+++ state.py
@@ -0,0 +1,7 @@
+
+class State():
+    def __init__(x,y):
+        self.x = x
+        self.y = y
+        self.coords = (x,y)
+        self.is_carrying = False
