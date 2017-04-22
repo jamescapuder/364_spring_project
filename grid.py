a4247480a825c0e18883c6eec97cfd20dceebed5
James Capuder <jamescapuder@ip-65-73.wireless.oberlin.edu>
2017-04-22 14:54:26 -0400

    grid init

---
+++ grid.py
@@ -0,0 +1,14 @@
+
+
+class Grid():
+
+    def __init__(self):
+        self.board = [[]]*50
+        self.board[0].append([0]*50)
+        
+        
+        
+
+if __name__=="__main__":
+    g = Grid()
+    print(g.board)
