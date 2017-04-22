0a82069413deebb9024c22363e343a11021949b7
James Capuder <jamescapuder@ip-65-73.wireless.oberlin.edu>
2017-04-22 17:44:29 -0400

    commented and stuff

--- grid.py
+++ grid.py
@@ -30,7 +30,8 @@ class Grid():
         return self.agents
     
     def do_action(self, agent, action):
-        x, y = agent.state
+        #Given an agent and a selected action, update the agent's state to reflect the change, return the reward
+        x, y = agent.state.coords
         new_x, new_y =  self.transitions[agent.state.coords][action]
         reward = self.generate_reward(agent, action)
         agent.state.updateCoords(new_x, new_y)
@@ -39,6 +40,10 @@ class Grid():
         return reward
 
     def generate_reward(self, agent, action):
+        #Given agent and action, returns the reward.
+        #Default is 0, agent neither picked up or dropped resources
+        #Reward is 10 if agent moves to source neighbor and agent.state.is_carrying == False (picks up resource)
+        #Reward is 20 if agent moves to spawn neighbor and agent.state.is_carrying == True (drops off resource)
         if self.getNextState(agent.state.coords, action) in list(self.getAdjacent(self.source)):
             if not agent.state.is_carrying:
                 agent.state.is_carrying = True
@@ -51,6 +56,7 @@ class Grid():
                 return 0
                     
     def get_agent_actions(self, agent):
+        #Given an agent, returns the list of valid actions given their current state 
         return list(self.transitions[agent.state.coords].keys())
         
     def transitionMap(self):
@@ -58,6 +64,7 @@ class Grid():
             self.transitions[state] = self.getAdjacent(state)
                 
     def getAdjacent(self, coord):
+        #Given an x,y coordinate tuple, returns a dictionary mapping actions to resulting coordinates. Invalid actions are removed from the dictionary
         adj =  {"right": (coord[0]+1, coord[1]), "left": (coord[0]-1, coord[1]), "down": (coord[0], coord[1]+1), "up": (coord[0], coord[1]-1)}
         for k in list(adj.keys() ):
             if (0 > adj[k][0]) or (adj[k][0]>49) or (0>adj[k][1]) or (adj[k][1]>49) or adj[k] in [self.source].extend([agent.state.coords for agent in self.agents]):
