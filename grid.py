748cb79e57c16543273ccd2faf6e74b4f379a9e3
James Capuder <jamescapuder@ip-65-73.wireless.oberlin.edu>
2017-04-24 20:39:59 -0400

    initialized member lists

--- grid.py
+++ grid.py
@@ -7,11 +7,11 @@ from source import Source
 class Grid():
 
     
-    def __init__(self, fpath):
+    def __init__(self, fpath, environment):
         self.agents = list()
         self.spawns = list()
         self.sources = list()
-        
+        self.environment = environment
         # TODO: read board from file
         preBoard = [list(l)[:-1] for l in open(fpath, 'r')]
         self.WIDTH = len(preBoard[0])-1
@@ -25,11 +25,16 @@ class Grid():
         
         self.initTileAdjacent()
 
-
     def initTileAdjacent(self):
         for x in self.board:
             for y in x:
                 y.initAdjacent(self)
+                if y.tile_type == Tile.SOURCE:
+                    self.sources.append(Source(y.x, y.y))
+                if y.tile_type == Tile.SPAWN:
+                    self.spawns.append(Spawn(y.x, y.y))
+                if y.tile_type == Tile.AGENT:
+                    self.agents.append(Agent(self.environment, State(y.x, y.y)))
                 
     def get_tile(self, x, y):
         return self.board[x][y]
