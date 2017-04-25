1943e035f741d2fe202bc7430e164f25a0a0f745
James Capuder <jamescapuder@ip-65-73.wireless.oberlin.edu>
2017-04-24 20:15:15 -0400

    initialize grid from file

--- tile.py
+++ tile.py
@@ -1,21 +1,26 @@
 
 class Tile:
-    def __init__(self, grid, x, y):
+    EMPTY = 'o'
+    SPAWN = 's'
+    SOURCE = 'r'
+    AGENT = 'a'
+    OBSTACLE = 'x'
+
+    def __init__(self, x, y, tile_type):
         self.x = x
         self.y = y
-        self.grid=grid
-        self.tileType = grid[x][y]
+        self.tile_type = tile_type
         self.adjacent = {}
-        self.initAdjacent()
+        
 
-    def initAdjacent(self):
+    def initAdjacent(self,grid):
         if self.y != 0:
-            self.adjacent["north"] = self.grid[self.x][self.y-1]
-        if self.y != self.grid.HEIGHT:
-            self.adjacent["south"] = self.grid[self.x][self.y+1]
+            self.adjacent["north"] = grid.board[self.x][self.y-1]
+        if self.y != grid.HEIGHT:
+            self.adjacent["south"] = grid.board[self.x][self.y+1]
         if self.x != 0:
-            self.adjacent["west"] = self.grid[self.x-1][self.y]
-        if self.x != self.grid.WIDTH:
-            self.adjacent["east"] = self.grid[self.x+1][self.y]
+            self.adjacent["west"] = grid.board[self.x-1][self.y]
+        if self.x != grid.WIDTH:
+            self.adjacent["east"] = grid.board[self.x+1][self.y]
     
         
